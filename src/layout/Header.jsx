9225d95a523c485bde0223d1ba0d7a7bdfc35b5e
nadezhda <kozlovskayani@yandex.ru>
2022-01-24 18:51:56 +0300

    gh-pages added

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,12 +2,18 @@ function Header() {
   return (
     <nav className="teal accent-4">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
+        <a href="/" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="!#">Repo</a>
+            <a
+              href="https://github.io/nkozlovskaya/react-shop"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Repo
+            </a>
           </li>
         </ul>
       </div>
